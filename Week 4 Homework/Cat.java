43b0a016e4910fa50a307f0356de42ae7aa7d330
Yuri <48239309+YuriSpiridonov@users.noreply.github.com>
2020-04-20 18:54:24 +0300

    Create Cat.java

---
+++ Week 4 Homework/Cat.java
@@ -0,0 +1,25 @@
+public class Cat extends Animal {
+    protected int runDistance = 200;
+    protected double jumpHeight = 2.0;
+    protected int SwimDistance = -1;
+
+    public Cat (String _name, int _run, double _jump, int _swim){
+        this.name = "untitled";
+        this.run = _run;
+        this.jump = _jump;
+        this.swim = _swim;
+
+    }
+    public Cat (int run, double jump) {
+    }
+    public boolean canRun(int run){
+        return run < runDistance ? true : false;
+    }
+    public boolean canJump(double jump){
+        return jump < jumpHeight ? true : false;
+    }
+    public boolean canSwim(int swim){
+        return swim < SwimDistance ? true : false;
+    }
+
+}
